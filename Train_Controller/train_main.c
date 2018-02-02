a2a6dcbac6751835bb41d895dc8a2a8ddbe504cb
GuyMaor <GuyAvrMaor@gmail.com>
2018-02-02 12:57:13 -0600

    I commented some of the train_main.c code.

--- Train_Controller/train_main.c
+++ Train_Controller/train_main.c
@@ -1,3 +1,20 @@
+/*******************************************************************************
+*	train_main.c
+*	Author: Joshua Newhouse
+*
+*	Tab width: 4
+*
+*	Purpose:
+*
+*	This implements the interface for controlling the train.
+*
+*	Procedures:
+*
+*	main				contains the beginning of the code.
+*	printMenu			displays a menu of key controls.
+*	setSpeed			prompts the user for setting a train speed.
+*******************************************************************************/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <conio.h>
@@ -38,14 +55,17 @@ int main(void) {
 		case 0:
 			break;
 		case 'w':
+			/* The train is sent a command to set its direction to forwards. */
 			target_setCommand(&train, TRAIN_FORWARD, 0);
 			base_sendData(&base, target_getCommand(&train));
 			break;
 		case 's':
+			/* The train is sent a command to set its direction to backwards. */
 			target_setCommand(&train, TRAIN_REVERSE, 0);
 			base_sendData(&base, target_getCommand(&train));
 			break;
 		case '*':
+			/* The user is first asked for a speed. Then, the train is sent a command to set its speed to what was given by the user. If the train reaches the maximum speed, the horn is honked. */
 			setSpeed(&train);
 			base_sendData(&base, target_getCommand(&train));
 			/////////////////////////////////////////////////////////
@@ -59,6 +79,7 @@ int main(void) {
 			printMenu();
 			break;
 		case '+':
+			/* The train is sent a command to increase its speed by 1. */
 		    if(current_speed < MAX_SPD) {
 				target_setCommand(&train, TRAIN_RELSPD, 6);
 				base_sendData(&base, target_getCommand(&train));
@@ -66,6 +87,7 @@ int main(void) {
 			}
 			break;
 		case '-':
+			/* The train is sent a command to decrease its speed by 1. */
 		    if(current_speed) {
 				target_setCommand(&train, TRAIN_RELSPD, 4);
 				base_sendData(&base, target_getCommand(&train));
@@ -73,36 +95,44 @@ int main(void) {
 			}
 			break;
 		case ' ':
+			/* The train is sent a command to use its breaks. This does not set the speed back to 0. */
 			target_setCommand(&train, TRAIN_BRAKE, 0);
 			base_sendData(&base, target_getCommand(&train));
 			break;
 		case 'b':
+			/* The train is sent a command to boost its speed. The train will return to its original speed once the boost has ended. */
 			target_setCommand(&train, TRAIN_BOOST, 0);
 			base_sendData(&base, target_getCommand(&train));
 			break;
 		case '1':
+			/* The train is sent a command to use horn 1. */
 			target_setCommand(&train, TRAIN_HORN1, 0);
 			base_sendData(&base, target_getCommand(&train));
 			break;
 		case '2':
+			/* The train is sent a command to use horn 2. */
 			target_setCommand(&train, TRAIN_HORN2, 0);
 			base_sendData(&base, target_getCommand(&train));
 			break;
 		case 'Q': case 'q':
+			/* This sends a command to terminates the program. */
 			target_setCommand(&train, SYSTEM_HALT, 0);
 			base_sendData(&base, target_getCommand(&train));
 			run = 0;
 			break;
 		case 'h':
+			/* The train is sent a command to set its speed to 0. */
 			target_setCommand(&train, TRAIN_ABSSPD, 0);
 			base_sendData(&base, target_getCommand(&train));
 			break;
 		case 't':
+			/* The train is sent a command to toggle its direction. This sets the speed to 0. */
 			target_setCommand(&train, TRAIN_TOGGLE, 0);
 			base_sendData(&base, target_getCommand(&train));
 			current_speed = 0;
 			break;
 		case 'r':
+			/* The train is sent a command to toggle its direction. The original speed is kept. */
 			target_setCommand(&train, TRAIN_TOGGLE, 0);
 			base_sendData(&base, target_getCommand(&train));
 			target_setCommand(&train, TRAIN_ABSSPD, current_speed);
@@ -118,6 +148,13 @@ int main(void) {
 	base_close(&base);
 	exit(EXIT_SUCCESS);
 }
+/*******************************************************************************
+*	void printMenu(void) 
+*
+*	Description:	Displays a menu for the user to know what keys to press.
+*					
+*
+*******************************************************************************/
 
 void printMenu(void) {
 	system("cls");
@@ -137,6 +174,17 @@ void printMenu(void) {
 		"q:\tQuit\n"
 	);
 }
+/*******************************************************************************
+*	void setSpeed(Target_ts* train) 
+*
+*	Description:	Prompts the user for a train speed. Then, it sets the speed for the train.
+*					on the target passed as the argument.
+*
+*	Parameters:
+*	train		The target object that contains the train information needed for controllin the train.
+*
+*	
+*******************************************************************************/
 
 void setSpeed(Target_ts* train) {
 	uint8_t spd;
